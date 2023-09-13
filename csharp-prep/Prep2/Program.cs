1df99ebc0f54d5ed8592ac6dc4c20b49a1b7b9bc
Mausham Bista <maushambista0@gmail.com>
2023-09-13 12:07:06 -0600

    grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade: ");
+        string grades = Console.ReadLine();
+        int grade = int.Parse(grades);
+
+        // if (grade >= 90)
+        // {
+        //     Console.WriteLine("You got an A");
+        // }
+        // else if (grade >= 80)
+        // {
+        //     Console.WriteLine("You got a B");
+        // }
+        // else if (grade >= 70)
+        // {
+        //     Console.WriteLine("You got a C");
+        // }
+        // else if (grade >= 60)
+        // {
+        //     Console.WriteLine("You got a D");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("You Failed.");
+        // }
+
+        if (grade>=70)
+        {
+            Console.WriteLine("Congrats you passed.");
+        }
+        else{
+            Console.WriteLine("Sorry, you failed.");
+        }
     }
 }
