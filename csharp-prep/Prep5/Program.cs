50675a1151d700b0f9f4e8084e251985000c6899
Mausham Bista <maushambista0@gmail.com>
2023-09-21 18:43:50 -0600

    done with functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+    static void DisplayProgram(){
+            Console.WriteLine("Welcome to the program!");
+        }
+    static string GetName(){
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+    static int GetNumber(){
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+    
+    static int SquareNum(int number){
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square){
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       DisplayProgram();
+       string name = GetName();
+       int number = GetNumber();
+       int square = SquareNum(number);
+       DisplayResult(name, square);
+
     }
 }
