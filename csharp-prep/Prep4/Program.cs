e4f873d3631b1063b00a759aeaf7370d881b0352
Mausham Bista <maushambista0@gmail.com>
2023-09-21 18:29:09 -0600

    completed the list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> numbers = new List<int>();
+
+        bool flag = true;
+         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (flag){        
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            numbers.Add(num);
+            if (num == 0){
+                flag = false;
+            }
+        }
+
+        int sum = 0;
+        int largest = 0;
+        int smallest_pos = int.MaxValue;
+        foreach(int number in numbers){
+            sum += number;
+            if (number > largest){
+                largest = number;
+            }
+            if (number > 0 && number < smallest_pos){
+                smallest_pos = number;
+            }
+        }
+        numbers.Sort();
+        int avg = sum/(numbers.Count - 1);
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest_pos}");
+        foreach(int number in numbers){
+            Console.WriteLine(number);
+        }
     }
 }
