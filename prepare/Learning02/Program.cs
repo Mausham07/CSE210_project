678866b283a2d40f41dd3803599fd3772f167ba0
Mausham Bista <maushambista0@gmail.com>
2023-09-25 18:33:35 -0600

    Coded all

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Machine Learning Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "09/12/2021";
+        job1._endYear = "12/09/2022";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Architecture";
+        job2._company = "Mausham PVT LTD";
+        job2._startYear = "09/12/2020";
+        job2._endYear = "12/09/2021";
+
+        Resume person1 = new Resume();
+        person1._name = "Mausham Bista";
+        person1._jobs.Add(job1);
+        person1._jobs.Add(job2);
+
+        person1.ResumeDisplay();
+
+        
+
     }
 }
