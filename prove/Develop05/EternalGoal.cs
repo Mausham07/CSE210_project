5580392e9d3a1ddd82d3455ee6356f3697dd363b
Mausham Bista <maushambista0@gmail.com>
2023-11-18 01:05:01 -0700

    checked

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,24 +1,29 @@
 class EternalGoal : Goal
 {
-    public  EternalGoal(string mb_name, string mb_description, int mb_points) : base(mb_name, mb_description, mb_points)
+    // Constructor
+    public EternalGoal(string mb_name, string mb_description, int mb_points) : base(mb_name, mb_description, mb_points)
     {
-        
+        // The constructor of the base class (Goal) is called with specified parameters
     }
+
+    // Override List method
     public override void List(int i)
-    { 
-        
+    {
         Console.WriteLine($"{i}. [ ] {base.getName()} ({base.getDescription()})");
-        
+        // This displays the eternal goal information in a specific format
     }
+
+    // Override Complete method
     public override int Complete()
-   {
-         return base.getPoints();
-         
-      }
+    {
+        return base.getPoints();
+        // Completing an eternal goal returns its base points without any additional logic
+    }
+
+    // Override SaveFile method
     public override string SaveFile()
     {
-        
         return $"EternalGoal,{base.getName()},{base.getDescription()},{base.getPoints()},{_mb_isCompleted}";
+        // This returns a string representation of the eternal goal for saving to a file
     }
-    
 }
